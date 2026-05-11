cb28f2c93aef0311820c86f5ccc1796a4f037779
25520108 <25520108@gm.uit.edu.vn>
2026-05-12 00:44:56 +0700

    removeline.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,44 +1,85 @@
-﻿#include <iostream>
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include <ctime>
 using namespace std;
 
-const int HEIGHT = 20;
-const int WIDTH = 10;
+#define H 20
+#define W 15
 
-int board[HEIGHT][WIDTH];
-int score = 0;
+char board[H][W] = {} ;
+char blocks[][4][4] = {
+        {{' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}},
+        {{' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}, {' ','I',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {'I','I','I','I'}, {' ',' ',' ',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','O','O',' '}, {' ','O','O',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','T',' ',' '}, {'T','T','T',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ','S','S',' '}, {'S','S',' ',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {'Z','Z',' ',' '}, {' ','Z','Z',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {'J',' ',' ',' '}, {'J','J','J',' '}, {' ',' ',' ',' '}},
+        {{' ',' ',' ',' '}, {' ',' ','L',' '}, {'L','L','L',' '}, {' ',' ',' ',' '}}
+};
+
+int x = 4, y = 0, b = 1;
+
+void gotoxy(int x, int y) {
+    COORD c = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void boardDelBlock() {
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' ' && y+j < H)
+                board[y+i][x+j] = ' ';
+}
+
+void block2Board() {
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' ' )
+                board[y+i][x+j] = blocks[b][i][j];
+}
 
 void initBoard() {
-
-    for (int i = 0; i < HEIGHT; i++) {
-        for (int j = 0; j < WIDTH; j++) {
-            board[i][j] = 0;
-        }
-    }
+    for (int i = 0 ; i < H ; i++)
+        for (int j = 0 ; j < W ; j++)
+            if ((i==H-1) || (j==0) || (j == W-1)) board[i][j] = '#';
+            else board[i][j] = ' ';
 }
 
-void printBoard() {
-
-    for (int i = 0; i < HEIGHT; i++) {
-
-        for (int j = 0; j < WIDTH; j++) {
-
-            if (board[i][j] == 0)
-                cout << ". ";
-            else
-                cout << "# ";
-        }
-
-        cout << endl;
-    }
-
-    cout << "\nScore: " << score << endl;
+void draw() {
+    gotoxy(0,0);
+    for (int i = 0 ; i < H ; i++, cout<<endl)
+        for (int j = 0 ; j < W ; j++)
+            cout << board[i][j];
 }
 
+bool canMove(int dx, int dy) {
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[b][i][j] != ' '){
+                int tx = x + j + dx;
+                int ty = y + i + dy;
+                if ( tx<1 || tx >= W-1 || ty >= H-1) return false;
+                if ( board[ty][tx] != ' ') return false;
+            }
+    return true;
+}
+
+// --- THUẬT TOÁN XÓA HÀNG CỦA SV2 ---
 void removeLine() {
-    // Chỉ chạy từ H-2 vì H-1 là viền đáy '#'
-    for (int i = H - 2; i >= 0; i--) {
+    // Chạy từ H-2 vì H-1 là viền đáy '#'
+    for (int i = H - 2; i > 0; i--) {
         bool full = true;
-
+        
         // Bỏ qua cột 0 và W-1 vì nó là viền '#'
         for (int j = 1; j < W - 1; j++) {
             if (board[i][j] == ' ') { // Nếu có khoảng trắng là chưa đầy
@@ -46,55 +87,59 @@ void removeLine() {
                 break;
             }
         }
-
-        // Nếu hàng đầy (không có khoảng trắng)
+            
         if (full) {
             // Kéo các hàng từ trên xuống
             for (int k = i; k > 0; k--) {
                 for (int j = 1; j < W - 1; j++) {
-                    board[k][j] = board[k - 1][j];
+                    board[k][j] = board[k-1][j];
                 }
             }
-
+                    
             // Dòng trên cùng (dòng 0) gán lại khoảng trắng
             for (int j = 1; j < W - 1; j++) {
                 board[0][j] = ' ';
             }
             
-            // TODO: Kết hợp với biến lineCount của SV5 ở đây
-            // lineCount++;
+            // Giữ lại hiệu ứng vẽ để thấy hàng biến mất
+            draw();
+            _sleep(100); 
 
-            // Kiểm tra lại dòng hiện tại (i) sau khi đã kéo xuống
+            // SV2 logic: Kiểm tra lại chính dòng này (phòng trường hợp ăn 2-3 hàng)
             i++; 
         }
     }
 }
-void createTestData() {
 
-    // dòng đầy
-    for (int j = 0; j < WIDTH; j++) {
-        board[19][j] = 1;
-    }
-
-    // block khác
-    board[18][2] = 1;
-    board[18][3] = 1;
-    board[17][5] = 1;
-}
-
-int main() {
-
+int main()
+{
+    srand(time(0));
+    b = rand() % 7;
+    system("cls");
     initBoard();
-
-    createTestData();
-
-    cout << "Before removeLine:\n\n";
-    printBoard();
-
-    removeLine();
-
-    cout << "\nAfter removeLine:\n\n";
-    printBoard();
-
+    
+    while (1){
+        boardDelBlock();
+        if (kbhit()){
+            char c = getch();
+            if (c=='a' && canMove(-1,0)) x--;
+            if (c=='d' && canMove(1,0) ) x++;
+            if (c=='x' && canMove(0,1))  y++;
+            if (c=='q') break;
+        }
+        
+        if (canMove(0,1)) y++;
+        else {
+            block2Board();
+            removeLine(); // Chạy hàm của SV2
+            x = 5; y = 0; b = rand() % 7; 
+        }
+        
+        block2Board();
+        draw();
+        
+        // Tốc độ được giữ nguyên ở mức 200ms ban đầu (chưa có code SV5)
+        _sleep(200); 
+    }
     return 0;
 }
